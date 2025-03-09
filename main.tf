3f791f56c6ee41949d5be4f9bab4d26717469890
Elad Chadad <elad25chadad@gmail.com>
2025-03-09 17:03:25 +0200

    add eks_access_policy

--- main.tf
+++ main.tf
@@ -32,6 +32,15 @@ resource "aws_route_table_association" "associate_subnet_two" {
   subnet_id      = local.subnet_ids[1]
   route_table_id = local.route_table_id
 }
+resource "aws_eks_access_policy_association" "eks_users_access" {
+  cluster_name  = var.cluster_name
+  policy_arn    = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  principal_arn = arn:aws:iam::730335218716:user/elad-user
+
+  access_scope {
+    type = "cluster"
+  }
+}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
