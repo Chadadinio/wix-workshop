016175e7ed48c344a51f9c7d67f172f052d66b70
Elad Chadad <elad25chadad@gmail.com>
2025-03-09 15:54:04 +0200

    add provider.tf file

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+      }
+      kubernetes = {
+        source  = "hashicorp/kubernetes"
+        version = "2.16.0"
+      }
+      kubectl = {
+        source  = "gavinbunney/kubectl"
+        version = "~>1.14"
+      }
+    }
+}
